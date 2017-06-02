e9ac77dadb866c5dec7f34e5d2518b642eaaa5c1
PAYVISION\e.perez <e.perez@payvision.com>
2017-06-02 11:12:56 +0200

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name camelRace20App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the camelRace20App
+ */
+angular.module('camelRace20App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name camelRace20App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the camelRace20App
+ */
+angular.module('camelRace20App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
