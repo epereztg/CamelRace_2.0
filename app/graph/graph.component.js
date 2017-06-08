cdf969bd7537255d0e74806f309b339b27a339fb
PAYVISION\e.perez <e.perez@payvision.com>
2017-06-08 12:02:48 +0200

    Passing parameter in  grapg component

--- app/graph/graph.component.js
+++ app/graph/graph.component.js
@@ -5,6 +5,13 @@
 angular.
 module("app").
 component("graph", {
+  bindings: {
+    width: '=',
+    height: '=',
+    yAxis: '=',
+    xAxis: '='
+  },
+
   templateUrl: 'graph/graph.template.html',
 
   controller: ['$http', 'TasksService', function graphController($http, TasksService) {
@@ -13,13 +20,23 @@ component("graph", {
       TasksService.getList()
         .then(function(data) {
           self.data = data;
-          self.width = 600;
-          self.height = 600;
-          self.yAxis = "Ongoing Tasks";
-          self.xAxis = "Race!";
+          // self.width = 600;
+          // self.height = 600;
+          // self.yAxis = "Ongoing Tasks";
+          // self.xAxis = "Race!";
           self.max = 0;
           var arrLength = self.data.length;
-          self.max = data.reduce((acc, task) => acc >= task.total ? acc : task.total, 0)
+
+          // "Arrow functions" are not supported by PhantomJS 2.x
+          // PhantomJS only supports a small subset of ES6.
+          // https://stackoverflow.com/questions/39026881/unexpected-token-using-array-some-with-karma-and-phantomjs
+          // self.max = data.reduce((acc, task) => acc >= task.total ? acc : task.total, 0)
+          var arrLength = self.data.length;
+          for (var i = 0; i < arrLength; i++) {
+            // Find Maximum X Axis Value
+            if (self.data[i].total > self.max)
+              self.max = self.data[i].total;
+          }
         });
 
     });
