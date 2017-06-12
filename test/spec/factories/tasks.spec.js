c1649c8aec0f31fd36e9f18d0233fc4a8381535e
PAYVISION\e.perez <e.perez@payvision.com>
2017-06-12 16:44:56 +0200

    Fixinf service test

--- test/spec/factories/tasks.spec.js
+++ test/spec/factories/tasks.spec.js
@@ -1,35 +1,58 @@
 'use strict';
 
 describe('Factory: TasksService', function() {
-  var myService = null;
-  var responseData = null;
-  var items = null;
+var myService = null;
+var responseData = null;
+var items = null;
+var $httpBackend;
 
-  beforeEach(module('core.services'));
+beforeEach(module('core.services'));
 
-  beforeEach(inject(function(_TasksService_) {
-    myService = _TasksService_;
-  }));
-  //});
-  //Example
-  var TasksService;
-  //Tasksservice
-  it('can get an instance', inject(function(TasksService) {
+// beforeEach(inject(function($injector) {
+//     // Set up the mock http service responses
+//     $httpBackend = $injector.get('$httpBackend');
+//
+//   }
 
-    expect(TasksService).toBeDefined();
+beforeEach(inject(function(_TasksService_) {
+  myService = _TasksService_;
+  $httpBackend = $injector.get('$httpBackend');
+}));
+//});
+//Example
+var TasksService;
+//Tasksservice
+it('can get an instance', inject(function(TasksService) {
 
-  }));
+  expect(TasksService).toBeDefined();
+}));
 
-  it('should return 12 items', function() {
-    console.log("AAAAAAAAAAA");
-    myService.getList()
-      .then(function(items) {
-        responseData = items;
-      });
-    console.log(items);
-    expect(responseData.length).toBe(12);
-    console.log("BBBBBBBBBBBN");
+it('should return 12 items', function() {
 
+  myService.getList()
+    .then(function(items) {
+      responseData = items;
+    });
+  console.log(items);
+  expect(responseData.length).toBe(12);
+
+
+});
+
+});
+e).toBeDefined();
+}));
+
+it('should return 12 items', function() {
+
+myService.getList()
+  .then(function(items) {
+    responseData = items;
   });
+console.log(items);
+expect(responseData.length).toBe(12);
+
+
+});
 
 });
